51d85ccb9aea37d676c1f12401eeae0860fddf7d
italostf <italostefaisk369@gmail.com>
2025-02-17 21:15:14 -0300

    import inicial

---
+++ projeto-italostefaisk/src/main/java/br/ue/desenvolvimento/web/projeto_italostefaisk/ProjetoItalostefaiskApplication.java
@@ -0,0 +1,13 @@
+package br.ue.desenvolvimento.web.projeto_italostefaisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoItalostefaiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoItalostefaiskApplication.class, args);
+	}
+
+}
---
+++ projeto-italostefaisk/src/test/java/br/ue/desenvolvimento/web/projeto_italostefaisk/ProjetoItalostefaiskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ue.desenvolvimento.web.projeto_italostefaisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoItalostefaiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
